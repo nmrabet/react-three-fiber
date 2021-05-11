750b48d4f0ec6d1f52b205fcada614bc3a1b987d
nesrine mrabet <mrabet_nesrine@yahoo.fr>
2021-05-11 10:13:11 +0100

    Added main folders

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+import React, {Suspense} from 'react';
 import './App.css';
+import { Canvas } from '@react-three/fiber';
+import Model from './components/three/Scene';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Canvas colorManagement shadowMap camera={{position: [-5, 4, 4], fov: 40}}>
+      <Suspense fallback={null}>
+        <Model />
+      </Suspense>
+    </Canvas>
+    </>
   );
 }
 
