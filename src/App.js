ed348bd9720cb3e09cd05a7a578536ac6518c321
nesrine mrabet <mrabet_nesrine@yahoo.fr>
2021-05-11 11:08:28 +0100

    Shoe display

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, {Suspense} from 'react';
 import './App.css';
+import {OrbitControls} from '@react-three/drei'
 import { Canvas } from '@react-three/fiber';
-import Model from './components/three/Scene';
+import Model from './Shoe-draco';
 import "./App.css";
 
 function App() {
   return (
     <>
-    <Canvas colorManagement shadowMap camera={{position: [-5, 4, 4], fov: 40}}>
+    <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 20 }}>
       <Suspense fallback={null}>
+        <ambientLight intensity={0.5}/>
         <Model />
       </Suspense>
+      <OrbitControls minPolarAngle={Math.PI / 2} maxPolarAngle={Math.PI / 2} enableZoom={false} enablePan={false} />
     </Canvas>
     </>
   );
